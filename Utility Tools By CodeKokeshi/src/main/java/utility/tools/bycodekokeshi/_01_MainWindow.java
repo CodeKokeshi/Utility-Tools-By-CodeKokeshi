db5d7855a8fd3fa25c722a0689aa06be6b5e6600
CodeKokeshi <112242924+CodeKokeshi@users.noreply.github.com>
2024-02-20 15:55:12 +0800

    _01_MainWindow.java

--- Utility Tools By CodeKokeshi/src/main/java/utility/tools/bycodekokeshi/_01_MainWindow.java
+++ Utility Tools By CodeKokeshi/src/main/java/utility/tools/bycodekokeshi/_01_MainWindow.java
@@ -1,21 +1,12 @@
 package utility.tools.bycodekokeshi;
 
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class _01_MainWindow implements Initializable {
     // GUI Components
