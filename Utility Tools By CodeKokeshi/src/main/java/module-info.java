8021f47310e10420f3b45885de26bd28c91e0d12
CodeKokeshi <112242924+CodeKokeshi@users.noreply.github.com>
2024-02-20 16:08:28 +0800

    Delete Utility Tools By CodeKokeshi directory

--- Utility Tools By CodeKokeshi/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.shutdownwithtimergui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.dlsc.formsfx;
-    requires java.prefs;
-    opens utility.tools.bycodekokeshi to javafx.fxml;
-    exports utility.tools.bycodekokeshi;
-}
--- Utility Tools By CodeKokeshi/src/main/java/utility/tools/bycodekokeshi/_00_MainClass.java
+++
@@ -1,8 +0,0 @@
-package utility.tools.bycodekokeshi;
-
-public class _00_MainClass {
-    // Main Class For Shaded Jar File
-    public static void main(String[] args) {
-        _00_Special.main(args);
-    }
-}
